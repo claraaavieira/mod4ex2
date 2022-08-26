d58a5dfd41bbd49c4f2837fa637f976cd2c25a27
claraaavieira <110980134+claraaavieira@users.noreply.github.com>
2022-08-26 08:20:38 -0300

    Add files via upload

---
+++ aRTIC.py
@@ -0,0 +1,11 @@
+class Arctic_Monkeys:
+  def __init__(self, arctic):
+    self.arctic = arctic
+
+
+  def exibir1(self):
+    print(self.arctic)
+
+
+ar = Arctic_Monkeys('TERÁ SHOW DO ARTIC MONKEYS NO BRASIL!!')
+ar.exibir1()
